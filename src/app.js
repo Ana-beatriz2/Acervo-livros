caa807567872b0de44ef6b1e3537fcb7b3f9bbb6
Ana-beatriz2 <caldas.ana9@gmail.com>
2024-01-06 18:55:31 -0300

    refactor: remoção de rotas antigas

--- src/app.js
+++ src/app.js
@@ -15,37 +15,4 @@ conexao.once("open", () => {
 const app = express();
 routes(app);
 
-app.get("/livros/:id", (req, res) => {
-    const { id } = req.params;
-
-    const index = buscarLivro(id);
-    return res.status(200).json(livros[index]);
-})
-
-app.post("/livros", (req, res) => {
-    livros.push(req.body);
-    res.status(201).json({messgae: "Livro cadastrado com sucesso!"});
-})
-
-app.put("/livros/:id", (req, res) => {
-    const { id } = req.params;
-    const { titulo } = req.body;
-
-    const index = buscarLivro(id);
-    livros[index].titulo = titulo;
-
-    return res.status(200).json(livros);
-})
-
-app.delete("/livros/:id", (req, res) => {
-    const { id } = req.params;
-
-    const index = buscarLivro(id);
-    livros.splice(index, 1);
-
-    res.status(200).json({message: "Livro deletado com sucesso!"});
-})
-
 export default app;
-
-// mongodb+srv://admin:jesusemeuamigo@cluster0.qcvstes.mongodb.net/livraria?retryWrites=true&w=majority
