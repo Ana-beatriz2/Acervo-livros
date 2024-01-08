9cb75397285a8c7be3af52fff07bf6749fce6e2b
Ana-beatriz2 <caldas.ana9@gmail.com>
2024-01-08 17:40:06 -0300

    feat: inicio do tratamento de erros em listar autor por id

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import { autor } from "../models/Autor.js";
 
 class AutorController {
@@ -8,18 +9,27 @@ class AutorController {
         } catch (error){
             return res.status(500).json({message: `${error.message} - Erro ao retornar autores!`});
         }
-    };
+    }
 
     static async listaAutor(req, res) {
         const { id } = req.params;
 
         try{
             const autorRetornado = await autor.findById(id);
-            return res.status(200).json(autorRetornado);
+
+            if (autorRetornado){
+                return res.status(200).json(autorRetornado);
+            } 
+            
+            return res.status(404).json({message: "Autor não encontrado!"});
         } catch (error){
+            if (error instanceof mongoose.Error.CastError){
+                return res.status(400).json({message: "Um ou mais dados fornecidos estão incorretos."});
+            }
+
             return res.status(500).json({message: `${error.message} - Erro ao retornar autor!`});
         }
-    };
+    }
 
     static async cadastraAutor(req, res){
         try{
@@ -28,7 +38,7 @@ class AutorController {
         } catch (error) {
             return res.status(500).json({message: `${error.message} - Erro ao criar autor!`});
         }
-    };
+    }
 
     static async atualizaAutor(req, res) {
         const { id } = req.params;
@@ -39,7 +49,7 @@ class AutorController {
         } catch (error){
             return res.status(500).json({message: `${error.message} - erro ao atualizar autor!`});
         }
-    };
+    }
 
     static async deletaAutor(req, res) {
         const { id } = req.params;
@@ -48,9 +58,9 @@ class AutorController {
             await autor.findByIdAndDelete(id);
             return res.status(200).json({message: "Autor deletado com sucesso!"});
         } catch (error){
-            return res.status(500).json({message: `${error.message} - erro ao deletar autor!`})
+            return res.status(500).json({message: `${error.message} - erro ao deletar autor!`});
         }
-    };
+    }
 }
 
 export default AutorController;
