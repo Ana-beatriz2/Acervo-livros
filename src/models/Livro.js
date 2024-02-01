b809a9eaa1cc98185e19698d0c41616a7e928811
Ana-beatriz2 <caldas.ana9@gmail.com>
2024-02-01 20:13:54 -0300

    feat(Livro): validação na quantidade de páginas

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -4,9 +4,9 @@ import { autorSchema } from "./Autor.js";
 const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
     titulo: { type: String, required: [true, "O título do livro é obrigatório."] },
-    editora: { type: String },
+    editora: { type: String /* enum: {values: ["Alura", "4 ventos"], message: "A editora {VALUE} não é permitida!"}*/},
     valor: { type: Number },
-    paginas: { type: Number },
+    paginas: { type: Number, min: [10, "{VALUE} não é um número de páginas entre 10 e 5000"], max: [5000, "{VALUE} não é um número de páginas entre 10 e 5000"]},
     autor: autorSchema // embedding
 }, { versionKey: false });
 
